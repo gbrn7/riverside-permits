71f103faeeb6fd414c6a440ae27c172012aa846b
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2026-09-23 07:41:15 +0700

    feat(backend): initialize Spring Boot project with H2 database and ISO-8601 config

---
+++ backend/src/main/java/uk/gov/riverside/permits/PermitRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.riverside.permits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermitRegisterApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/uk/gov/riverside/permits/PermitRegisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.riverside.permits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermitRegisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
